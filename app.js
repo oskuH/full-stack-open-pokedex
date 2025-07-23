fc03304b7f4c63199a477cc39bf5bc1edb03cdbe
Oskari <104070288+oskuH@users.noreply.github.com>
2025-07-23 14:20:57 +0300

    Test failed build Discord notification

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 app.get('/version', (req, res) => {
   res.send('5')
